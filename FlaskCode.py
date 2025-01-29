77d907ee38de0dcb05a2edad1a73293e75be41f7
Meghraj Wagh <meghrajwagh2004@gmail.com>
2025-01-29 12:23:32 +0530

    minor changes in FlaskCode.py

--- FlaskCode.py
+++ FlaskCode.py
@@ -1,8 +1,8 @@
 from flask import *
+import time
 import requests
 import os
-from dotenv import load_dotenv, find_dotenv
-from pathlib import Path
+from dotenv import load_dotenv 
 
 
 load_dotenv("API_KEY.env")
@@ -12,6 +12,7 @@ app = Flask(__name__)
 
 
 API_KEY = os.getenv('API_KEY')
+print(API_KEY)
 
 def generate_access_token(api_key):
     url = "https://iam.cloud.ibm.com/identity/token"
@@ -36,7 +37,7 @@ def index():
 
 # Function to send resume to IBM API
 def invoke_ibm_api(cv_data, iam_token):
-    url = "https://au-syd.ml.cloud.ibm.com/ml/v1/deployments/3dc4844a-704e-42de-8488-9f9608224f6b/text/generation?version=2021-05-01"
+    url = "https://au-syd.ml.cloud.ibm.com/ml/v1/deployments/f008ef55-55b2-41b6-9e78-1ac4a9f6f89a/text/generation?version=2021-05-01"
     
     headers = {
         "Content-Type": "application/json",
@@ -54,19 +55,22 @@ def invoke_ibm_api(cv_data, iam_token):
     return response.json()
 
 @app.route('/submit_cv', methods=['POST'])
+
 def submit_cv():
+    time.sleep(4)
     # Get the resume (CV) content from the request
     cv_data = request.form.get('data_cv')
-    #print(cv_data)
-
     iam_token = generate_access_token(API_KEY)
+    print(f' IAM_TOKEN {iam_token}')
    
     api_response = invoke_ibm_api(cv_data, iam_token)
+    print(f' API_RESPONSE {api_response}')
     
     # Extract questions from the generated text
     questions = api_response.get("results", [])[0].get("generated_text", "")
+    print(f'QUESTIONS {questions}')
     formatted_questions = questions.strip()  
-    # print(formatted_questions)
+    print(formatted_questions)
     
     # Return the questions to the frontend
     return render_template("index.html", questions=formatted_questions)
